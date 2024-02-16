0d3fcc4f9fac001604005e987da0be98b423f106
bogdan <harysh.bogdan@gmail.com>
2024-02-16 20:17:44 +0200

    initial commit

---
+++ src/main/java/org/example/b9routeridemanager/B9RouteRideManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.b9routeridemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B9RouteRideManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B9RouteRideManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/b9routeridemanager/B9RouteRideManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.b9routeridemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B9RouteRideManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
